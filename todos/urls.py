077218139373590bca17950218140eeda7a351aa
Lennox Mountain <lennoxmountain67@gmail.com>
2024-08-21 01:04:33 +0200

    restarted app. with minor adjustments

--- todos/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import TodoListView, TodoDetailView
-
-urlpatterns = [
-    path('', TodoListView.as_view(), name='todo_list'),
-    path('<int:pk>/', TodoDetailView.as_view(), name='todo_detail'),
-]
