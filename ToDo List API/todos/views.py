b53eb7528533ede9829f06e467b83a07447a24ee
Lennox Mountain <lennoxmountain67@gmail.com>
2024-08-21 01:53:43 +0200

    created serializers and views for todos app

--- ToDo List API/todos/views.py
+++ ToDo List API/todos/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .serializers import TodoSerializer
+from .permissions import IsAuthor
+from .models import Todo
+
 
 # Create your views here.
+
+class TodoListView(generics.ListAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
+
+class TodoDetailView(generics.RetrievUpdateDestroyAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
